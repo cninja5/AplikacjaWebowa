06df25c76fd9f017ac758167b97213732f78beed
Jacek22 <jacek.sidzina@student.up.krakow.pl>
2023-11-26 16:49:49 +0100

    Dodanie obsługi adminDashboard

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Uzytkownicy,DaneUzytkownicy,Znajomi,Listy,Prezent
+
+admin.site.register(Uzytkownicy)
+admin.site.register(DaneUzytkownicy)
+admin.site.register(Znajomi)
+admin.site.register(Listy)
+admin.site.register(Prezent)
 
 # Register your models here.
