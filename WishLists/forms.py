21b21bf73ede6c497399eaeabaa2357455c237ca
Jacek22 <jacek.sidzina@student.up.krakow.pl>
2023-11-27 22:04:42 +0100

    Zmiany wyglądowe

--- WishLists/forms.py
+++ WishLists/forms.py
@@ -7,3 +7,10 @@ class ListyForm(forms.ModelForm):
     class Meta:
         model = Listy
         fields = ['tytul', 'opis']
+        labels = {
+            'tytul': 'Nazwa listy',
+            'opis': 'Opis listy'
+        }
+        widgets = {
+            'opis': forms.Textarea(attrs={'rows': 4, 'cols': 50})  # Ustawienia dla pola textarea
+        }
