f688da02f32463789b9fa7f0da1a7b8062a6d53b
Jacek22 <jacek.sidzina@student.up.krakow.pl>
2023-11-27 19:11:34 +0100

    Dodanie modelu do list

--- main/models.py
+++ main/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 
+
 # Create your models here.
 RodzajePlci = (
     ('M', 'Mężczyzna',),
@@ -26,16 +27,24 @@ class Uzytkownicy(AbstractUser):
     )
 
 class Znajomi(models.Model):
-    idOsoba1 = models.ForeignKey(Uzytkownicy,on_delete=models.CASCADE)
+    idOsoba1 = models.ForeignKey(Uzytkownicy, on_delete=models.CASCADE)
     idOsoba2 = models.CharField(max_length=30)
 
 class Listy(models.Model):
-    loginWlasciciel = models.ForeignKey(Uzytkownicy,on_delete=models.CASCADE)
+    loginWlasciciel = models.ForeignKey(Uzytkownicy, on_delete=models.CASCADE)
     tytul = models.CharField(max_length=50)
     opis = models.CharField(max_length=300)
-    dataStworzenia = models.DateField(auto_now_add=True)
+    dataUtworzenia = models.DateField(auto_now_add=True)
+    zawartosc = models.ManyToManyField('ZawartoscListy', related_name='zawartoscListy', blank=True)
 
 class Prezent(models.Model):
-    idListy = models.ForeignKey(Listy,on_delete=models.CASCADE)
-    nazwa = models.CharField(max_length=254)
+    idListy = models.ForeignKey(Listy, on_delete=models.CASCADE)
+    nazwaPrezentu = models.CharField(max_length=254)
     loginRezerwacji = models.IntegerField()
+
+class ZawartoscListy(models.Model):
+    idListy = models.ForeignKey(Listy, on_delete=models.CASCADE)
+    nazwaPrezentu = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.nazwaPrezentu
