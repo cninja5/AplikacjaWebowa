af9876f88a33f61d7dc6dd0e0fbe1e0ee2082f3a
jakub_perczynski <jakub.perczynski@student.up.krakow.pl>
2023-11-29 18:44:58 +0100

    userProfiles - wyświetlanie danych użytkownika na którego profilu jesteśmy

--- UserProfiles/views.py
+++ UserProfiles/views.py
@@ -1,6 +1,14 @@
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
 # Create your views here.
 
 def profile(request, username):
-    return render(request, 'profile/profile.html')
+    user = get_object_or_404(User, username=username)
+
+    user_firstname = user.first_name
+    user_lastname = user.last_name
+    user_date_joined = user.date_joined
+    userdata = {'user_username': username, 'user_firstname': user_firstname, 'user_lastname': user_lastname, 'user_date_joined': user_date_joined}
+    return render(request, 'profile/profile.html', userdata)
