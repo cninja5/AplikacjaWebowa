4b40663eb3a1f1165dcf7813f359e5e981dc6616
jakub_perczynski <jakub.perczynski@student.up.krakow.pl>
2023-12-11 17:56:40 +0100

    walidacja awatarów, która nie działa

--- UserProfiles/forms.py
+++ UserProfiles/forms.py
@@ -1,3 +1,4 @@
+from PIL import Image
 from django import forms
 from main.models import ProfilUzytkownika
 
@@ -8,3 +9,15 @@ class editAvatarForm(forms.ModelForm):
     class Meta:
         model = ProfilUzytkownika
         fields = ['avatar']
+
+        def clean_picture(self):
+            avatar = self.cleaned_data.get("avatar")
+
+            if avatar:
+                image = Image.open(avatar)
+                width, height = image.size
+
+                if width != 400 or height != 400:
+                    raise forms.ValidationError("Awatar musi mieć rozdzielczość 400x400px!")
+
+            return avatar
